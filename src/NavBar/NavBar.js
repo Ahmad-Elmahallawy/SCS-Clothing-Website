e084a4d0bf2fd9f97fc520028ed6cb185727c7e9
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2023-03-25 18:57:52 -0400

    finished the NavBar

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../Styles/NavBarStyle.css";
+
+function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  return (
+    <header>
+      <img src="../Images/SCSLogo.png" alt="" />
+      <nav ref={navRef}>
+        <a href="/#">Home</a>
+        <a href="/#">All Clothing</a>
+        <a href="/#">Contact Us</a>
+        <a href="/#">About Us</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
+}
+
+export default Navbar;
